2038fc4ab0b4d1396b621ebe1b6d7b628f1f47b6
Kionte Jones <jkionte20@gmail.com>
2021-01-15 08:44:14 -0500

    created server w/mongodb atlas connection. added routes and models with relevant files.

---
+++ routes/api-routes.js
@@ -0,0 +1,33 @@
+const workout = require("..models/workout.js")
+
+module.exports = function(app) {
+    app.get("/api/workouts", function(req, res) {
+        workout.find()
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+    });
+
+    app.post("/api/workouts", function(req, res) {
+        workout.create({})
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+    });
+
+    app.get("/api/workouts", function(req, res) {
+        workout.find()
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+    });
+}
