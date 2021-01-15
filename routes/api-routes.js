a6f8cf119d51d9fbd1204dfcfa8acf3edaf4abab
Kionte Jones <jkionte20@gmail.com>
2021-01-15 08:56:56 -0500

    completed api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -30,4 +30,35 @@ module.exports = function(app) {
             res.json(err)
         })
     });
-}
+
+    app.get("/api/workouts/range",function(req,res){  
+        workout.find()
+        .then(data =>{  
+            res.json(data)
+        })
+        .catch(err => { 
+            res.json(err)
+        })
+    });
+
+
+    app.post("/api/workouts/range",function (req,res){    
+        workout.create({})
+        .then(data => res.json(data))
+        .catch(err => { 
+            res.json(err)
+        })
+    });
+
+    app.put("/api/workouts/:id",({body,params},res)=>{   
+        workout.findByIdAndUpdate(  
+         params.id,
+         {$push:{exercises:body} },
+         {new: true,runValidators:true }
+        )
+        .then(data => res.json(data))
+        .catch(err => { 
+            res.json(err)
+        })
+    });
+};
